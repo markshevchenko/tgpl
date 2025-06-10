cf9eab3aa3113fecb5e0057d6176b776a313cb50
Mark Shevchenko <markshevchenko@gmail.com>
2025-06-10 06:13:35 +0300

    Структуры

--- ch4/sha256/sha256.go
+++ ch4/sha256/sha256.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-	c1 := sha256.Sum256([]byte{'x'})
-	c2 := sha256.Sum256([]byte{'X'})
+	c1 := sha256.Sum256([]byte("x"))
+	c2 := sha256.Sum256([]byte("X"))
 
 	fmt.Printf("%x\n%x\n%t\n%T", c1, c2, c1 == c2, c1)
 }
